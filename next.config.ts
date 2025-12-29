e95806f10f6ea064f9e71d04ca380ab5d7ef2f96
thanakornnarawee <thanakornnarawee@gmail.com>
2025-12-30 01:59:36 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! คำเตือน: บรรทัดนี้จะสั่งให้ Vercel ยอมให้ Build ผ่านแม้จะมี Type Error !!
+    // สั่งให้ข้ามการตรวจ Error ของ TypeScript ตอน Build
+    // วิธีนี้จะทำให้คุณได้ URL เว็บไป Pitch งานแน่นอน 100%
     ignoreBuildErrors: true,
   },
   eslint: {
     // สั่งให้ข้ามการตรวจ ESLint ด้วยเพื่อความรวดเร็ว
     ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
